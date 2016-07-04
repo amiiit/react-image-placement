aa3b61389e9d6592c3caf39664609c8d3796f65d
Amit Jakubowicz <aj@hubrick.com>
2016-07-04 14:56:40 +0200

    css modules

---
+++ example/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import ImagePlacement from '../src/image-placement.jsx';
+import css from './App.less';
+
+const placementStyle = {
+    width: '100%',
+    height: '250px'
+}
+
+export default class App extends Component {
+    render () {
+        return (
+            // Add your component markup and other subcomponent references here.
+            <div className={css.app}>
+                <ImagePlacement
+                    currentImageSrc="https://d3v07npo1k6y3q.cloudfront.net/fd0da27f-c069-4761-bbd3-6b8ff89a2430"
+                    style={placementStyle}
+                />
+            </div>
+        );
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -45,31 +45,38 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  entry: [
-    'webpack-dev-server/client?http://localhost:5000',
-    'webpack/hot/dev-server',
-    './scripts/index'
-  ],
-  output: {
-    path: __dirname,
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  resolve: {
-    extensions: ['', '.js']
-  },
-  devtool: 'eval-source-map',
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        loaders: ['babel'],
-        include: path.join(__dirname, 'scripts')
-      }
-    ]
-  }
+    entry: [
+        'webpack-dev-server/client?http://localhost:5000',
+        'webpack/hot/dev-server',
+        './example/index'
+    ],
+    output: {
+        path: __dirname,
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    resolve: {
+        extensions: ['', '.js']
+    },
+    devtool: 'eval-source-map',
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loaders: ['babel'],
+                include: [
+                    path.join(__dirname, 'example'),
+                    path.join(__dirname, 'src')
+                ]
+            },
+            {
+                test: /\.less$/,
+                loader: "style!css?module&localIdentName=[name]__[local]!less"
+            }
+        ]
+    }
 };
